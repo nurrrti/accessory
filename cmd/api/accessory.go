53a0db9df91c53d21cac6e6671ac471e6323f855
Nurtay <nurtajm@MacBook-Pro-Nurtaj.local>
2023-02-22 23:41:08 +0600

    Initial commit

---
+++ cmd/api/accessory.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
+// return a plain-text placeholder response.
+func (app *application) createAccessoryHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new accessory")
+}
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve // the interpolated "id" parameter from the current URL and include it in a placeholder // response.
+
+func (app *application) showAccessoryHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "show the details of accessory %d\n", id)
+}
